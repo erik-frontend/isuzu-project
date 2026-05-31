c00813c88a8c926bb60a359ca3af87cd2d9fb930
Erik <130284367+erik-frontend@users.noreply.github.com>
2026-05-31 23:06:03 +0200

    commit
    add scripts

---
+++ js/category.js
@@ -0,0 +1,103 @@
+const categoryList = {
+
+    items: document.querySelectorAll('.category-list__item'),
+    init() {
+        if (!this.items.length) return;
+        this.items.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault()
+                this.items.forEach(el => {
+                    el.classList.remove('active')
+                });
+                item.classList.add('active')
+            })
+        })
+    }
+};
+
+const filterGroup = {
+    headers: document.querySelectorAll('.filter-group__header'),
+
+    init() {
+        this.headers.forEach(header => {
+            header.addEventListener('click', () => {
+                header.closest('.filter-group').classList.toggle('active');
+            })
+        })
+    }
+};
+
+const priceRange = {
+    range: document.querySelector('.price-filter__range'),
+    input: document.querySelector('.price-filter__input'),
+
+    init() {
+        noUiSlider.create(this.range, {
+            start: [0, 2500],
+            connect: true,
+
+            range: {
+                min: 0,
+                max: 5000
+            }
+        });
+
+        this.range.noUiSlider.on('update', (values) => {
+            this.input.value =
+                Math.round(values[1]) + ' грн';
+        });
+    }
+};
+
+const loadMore = {
+    cards: document.querySelectorAll('.catalog-grid .productCard'),
+    button: document.querySelector('.load-more-btn'),
+
+    visibleCount: 9,
+    step: 3,
+
+    init() {
+
+        if (!this.button || !this.cards.length) return;
+
+        this.cards.forEach((card, index) => {
+
+            if (index >= this.visibleCount) {
+                card.hidden = true;
+            }
+
+        });
+
+        this.button.addEventListener('click', () => {
+            this.showMore();
+        });
+
+    },
+
+    showMore() {
+
+        for (
+            let i = this.visibleCount;
+            i < this.visibleCount + this.step;
+            i++
+        ) {
+
+            if (this.cards[i]) {
+                this.cards[i].hidden = false;
+            }
+
+        }
+
+        this.visibleCount += this.step;
+
+        if (this.visibleCount >= this.cards.length) {
+            this.button.style.display = 'none';
+        }
+
+    }
+};
+
+categoryList.init()
+filterGroup.init()
+priceRange.init()
+loadMore.init()
